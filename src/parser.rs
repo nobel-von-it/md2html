94cb5b5780fb8cc3653f9ef330e1f9e543cfb2ec
nobel-von-it <maksimdavydenko12@gmail.com>
2025-06-08 14:09:33 +0700

    Start parser development

---
+++ src/parser.rs
@@ -0,0 +1,14 @@
+use crate::token::Token;
+
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
+}
+
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Parser {
+        Parser { tokens, current: 0 }
+    }
+
+    pub fn parse(&mut self) -> Vec<Stmt> {}
+}
